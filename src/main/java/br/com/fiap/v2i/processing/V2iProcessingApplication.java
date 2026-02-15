04c6c9c7c8eac5986021a510399279497ebcdcb3
Lucas Peixoto de Alencar Rocha <lucas.peixoto.14@gmail.com>
2026-02-15 14:21:12 -0300

    init spring boot

---
+++ src/main/java/br/com/fiap/v2i/processing/V2iProcessingApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.v2i.processing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class V2iProcessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(V2iProcessingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/v2i/processing/V2iProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.v2i.processing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class V2iProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
