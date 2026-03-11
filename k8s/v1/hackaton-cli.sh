f3714b1265c7b3a0e415f815e908fb42f19f3680
Edir Lucas Icety <icetylucas@gmail.com>
2026-03-10 21:24:58 -0300

    nome do arquivo de setup

--- k8s/v1/hackaton-cli.sh
+++ k8s/v1/hackaton-cli.sh
@@ -7,7 +7,7 @@ YELLOW='\033[1;33m'
 BLUE='\033[0;34m'
 NC='\033[0m'
 
-echo "🚀 Setup Tech Food Kubernetes"
+echo "🚀 Video Processing Setup"
 
 # Verificar se minikube está rodando
 if ! kubectl get nodes &> /dev/null; then
