095efc235688341b8a7b158621918c7fd2ddfbde
Lucas Peixoto de Alencar Rocha <lucas.peixoto.14@gmail.com>
2026-03-13 12:37:15 -0300

    refactor: integrate RabbitTemplate for video processing notifications

--- src/main/java/br/com/fiap/v2i/processing/video/VideoController.java
+++ src/main/java/br/com/fiap/v2i/processing/video/VideoController.java
@@ -2,6 +2,7 @@ package br.com.fiap.v2i.processing.video;
 
 import br.com.fiap.v2i.processing.client.V2iWebClient;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,11 +14,13 @@ public class VideoController {
     private final VideoProcessingService videoProcessingService;
     private final VideoDownloadService videoDownloadService;
     private final V2iWebClient v2iWebClient;
+    private final RabbitTemplate rabbitTemplate;
 
-    public VideoController(VideoProcessingService videoProcessingService, VideoDownloadService videoDownloadService, V2iWebClient v2iWebClient) {
+    public VideoController(VideoProcessingService videoProcessingService, VideoDownloadService videoDownloadService, V2iWebClient v2iWebClient, RabbitTemplate rabbitTemplate) {
         this.videoProcessingService = videoProcessingService;
         this.videoDownloadService = videoDownloadService;
         this.v2iWebClient = v2iWebClient;
+        this.rabbitTemplate = rabbitTemplate;
     }
 
     @PostMapping(value = "/extract-frames", consumes = MediaType.APPLICATION_JSON_VALUE)
@@ -30,6 +33,7 @@ public class VideoController {
 
         try (DownloadedVideo downloadedVideo = videoDownloadService.download(request.url(), request.filenameFromUrl())) {
             videoProcessingService.extractFramesAndStream(downloadedVideo.path(), response.getOutputStream());
+            rabbitTemplate.convertAndSend("video.processed", request.videoHashFromUrl());
             v2iWebClient.markAsComplete(request.videoHashFromUrl());
         } catch (IOException e) {
             v2iWebClient.markAsError(request.videoHashFromUrl());
